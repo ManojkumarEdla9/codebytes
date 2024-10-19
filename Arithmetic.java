9cd7f8e4db47816fc7492929fe42fdee932975f2
Manoj Kumar Edla <126246168+ManojkumarEdla9@users.noreply.github.com>
2024-10-19 14:41:53 +0530

    Add files via upload
    Second Commit

---
+++ Arithmetic.java
@@ -0,0 +1,13 @@
+package com.nt.service;
+
+public class Arithmetic {
+public int sum(int x,int y)
+{
+	return x+y;
+}
+
+public float sum(float h, float w) {
+	// TODO Auto-generated method stub
+	return h+w;
+}
+}
